9f8cf790c5145f6d1b28ee9f002c96822c5cb51f
ElitoSear <elitogamer100@gmail.com>
2025-03-09 12:08:32 -0600

    Initial commit

---
+++ src/main/java/elito/collision/Collision.java
@@ -0,0 +1,10 @@
+package elito.collision;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Collision implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
