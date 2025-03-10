4be6229622d9f8ff86bc1b0ba6b8930999a8e894
Edward <evelasquez0@ucol.mx>
2025-03-09 19:34:08 -0600

    Added condition. Known issues: Is block position not position.

--- src/main/java/elito/collision/mixin/ExecuteCommandMixin.java
+++ src/main/java/elito/collision/mixin/ExecuteCommandMixin.java
@@ -2,22 +2,24 @@ package elito.collision.mixin;
 
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.tree.CommandNode;
+import net.minecraft.block.BlockState;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.BlockPosArgumentType;
-import net.minecraft.command.argument.PosArgument;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ExecuteCommand;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.function.BooleanBiFunction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShapes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Position;
-import net.minecraft.server.command.ExecuteCommand.add
+import net.minecraft.util.math.Vec3d;
 
 @Mixin(ExecuteCommand.class)
 public class ExecuteCommandMixin {
@@ -30,9 +32,9 @@ public class ExecuteCommandMixin {
 
         argumentBuilder
                 .then(
-                        CommandManager.literal("loaded")
+                        CommandManager.literal("collision")
                                 .then(
-                                        addConditionLogic(
+                                        ExecuteCommand.addConditionLogic(
                                                 root,
                                                 CommandManager.argument("pos", BlockPosArgumentType.blockPos()),
                                                 positive,
@@ -43,7 +45,16 @@ public class ExecuteCommandMixin {
     }
 
     @Unique
-    private static void isColliding(ServerWorld world, Position position) {
+    private static boolean isColliding(ServerWorld world, BlockPos blockPos) {
+        double d = 0.01;
 
+        BlockState blockState = world.getBlockState(blockPos);
+
+
+        Box box = Box.of(new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()), d, d, d);
+        return !blockState.isAir()
+                && VoxelShapes.matchesAnywhere(
+                blockState.getCollisionShape(world, blockPos).offset(blockPos.getX(), blockPos.getY(), blockPos.getZ()), VoxelShapes.cuboid(box), BooleanBiFunction.AND
+        );
     }
 }
