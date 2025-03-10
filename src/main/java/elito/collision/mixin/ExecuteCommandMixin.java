8736715d2e26836fa9531b6e4720bbe1b5e445bf
ElitoSear <elitogamer100@gmail.com>
2025-03-10 02:55:57 -0600

    Replaced blockPos with vec3d so the argument accepts different positions rather than the origin of the context block.

--- src/main/java/elito/collision/mixin/ExecuteCommandMixin.java
+++ src/main/java/elito/collision/mixin/ExecuteCommandMixin.java
@@ -3,23 +3,25 @@ package elito.collision.mixin;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.CommandNode;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.command.CommandRegistryAccess;
-import net.minecraft.command.argument.BlockPosArgumentType;
+import net.minecraft.command.argument.Vec3ArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ExecuteCommand;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShapes;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Vec3d;
 
 @Mixin(ExecuteCommand.class)
 public class ExecuteCommandMixin {
@@ -36,25 +38,26 @@ public class ExecuteCommandMixin {
                                 .then(
                                         ExecuteCommand.addConditionLogic(
                                                 root,
-                                                CommandManager.argument("pos", BlockPosArgumentType.blockPos()),
+                                                CommandManager.argument("pos", Vec3ArgumentType.vec3()),
                                                 positive,
-                                                commandContext -> isColliding(commandContext.getSource().getWorld(), BlockPosArgumentType.getBlockPos(commandContext, "pos"))
+                                                commandContext -> isColliding(commandContext.getSource().getWorld(), Vec3ArgumentType.getVec3(commandContext, "pos"))
                                         )
                                 )
                 );
     }
 
     @Unique
-    private static boolean isColliding(ServerWorld world, BlockPos blockPos) {
+    private static boolean isColliding(ServerWorld world, Vec3d vec3d) {
         double d = 0.01;
 
+        BlockPos blockPos = new BlockPos((int) vec3d.getX(), (int) vec3d.getY(), (int) vec3d.getZ());
         BlockState blockState = world.getBlockState(blockPos);
 
 
-        Box box = Box.of(new Vec3d(blockPos.getX(), blockPos.getY(), blockPos.getZ()), d, d, d);
+        Box box = Box.of(new Vec3d(vec3d.getX(), vec3d.getY(), vec3d.getZ()), d, d, d);
         return !blockState.isAir()
                 && VoxelShapes.matchesAnywhere(
-                blockState.getCollisionShape(world, blockPos).offset(blockPos.getX(), blockPos.getY(), blockPos.getZ()), VoxelShapes.cuboid(box), BooleanBiFunction.AND
+                blockState.getCollisionShape(world, blockPos).offset(vec3d.getX(), vec3d.getY(), vec3d.getZ()), VoxelShapes.cuboid(box), BooleanBiFunction.AND
         );
     }
 }
